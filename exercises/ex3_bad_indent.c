2c86a56806535c01f666dc55ee749031d1d09a4a
Robert Quitt <robertquitt@gmail.com>
2018-09-17 15:40:56 -0700

    Add exercises

---
+++ exercises/ex3_bad_indent.c
@@ -0,0 +1,33 @@
+
+// fix the indendation using << and >>
+// use v+motion+< for many lines
+// type :help shift-left-right for help
+// try playing with set shiftround and set shiftwidth
+// (don't worry about what this code does)
+
+#include<stdio.h>
+
+int main() {
+    while (1) {
+    if (cond){
+    printf("Lorum");
+    printf("Ipsum");
+    printf("Dolor");
+    printf("Sit");
+    printf("Amet");
+    } else {
+                printf("Lorum");
+                printf("Ipsum");
+                printf("Dolor");
+                printf("Sit");
+                printf("Amet");
+    }
+    }
+    return 0;
+}
+
+void doSomething() {
+    // make me do something by moving some code over from main() !
+    // hint, use shift-V and d, then p
+    return 0;
+}
---
+++ exercises/ex5_whitespace.py
@@ -0,0 +1,17 @@
+    
+# there are a number of ways of solving this.
+# i wont give them away tho!
+# 
+	
+def main():
+        print("hello")
+	print("whitespace")   
+	print("can you find them all?")
+
+    	return 0
+
+	
+
+if __name__ == '__main__':
+	main()  
+	
